89f112c4e01a790565597b9c41ed3d033db26621
Giles Hall <giles@ginkgobioworks.com>
2016-04-21 20:00:57 -0400

    more work on tasks

--- Dockerfile
+++ Dockerfile
@@ -1,43 +1,15 @@
 FROM ubuntu:14.04
 MAINTAINER Giles Hall
 
-RUN apt-get update && \
-    apt-get install -y build-essential git default-jre-headless
-RUN apt-get install -y zlib1g-dev unzip wget python-dev python-pip samtools libcurl4-openssl-dev
-
-ENV SRC_DIR="/tmp/src"
-RUN mkdir $SRC_DIR
-
-# BWA
-ENV BWA_VERSION="v0.7.13"
-RUN git clone -b $BWA_VERSION https://github.com/lh3/bwa.git $SRC_DIR/bwa && \
-    cd $SRC_DIR/bwa && \
-    make && \
-    cp bwa /usr/local/bin
-
-# Sickle
-ENV SICKLE_VERSION="v1.33"
-RUN git clone -b $SICKLE_VERSION https://github.com/najoshi/sickle.git $SRC_DIR/sickle && \
-    cd $SRC_DIR/sickle && \
-    make && \
-    cp sickle /usr/local/bin
-
-# FastQC
-RUN cd $SRC_DIR && \
-    wget http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.5.zip && \
-        unzip fastqc_v0.11.5.zip && \
-        chmod 755 FastQC/fastqc && \
-        mv FastQC /usr/local/share && \
-        ln -s ../share/FastQC/fastqc /usr/local/bin/fastqc
-
-# cleanup
-RUN rm -rf $SRC_DIR
+COPY /scripts/build_image.sh /build_image.sh
+RUN /build_image.sh && rm /build_image.sh
 
 # Bones
-ENV BONES_VERSION="master"
 RUN pip install pysam celery requests && \
     pip install https://github.com/vishnubob/ssw/archive/master.zip
 COPY / /bones
 
+RUN chown -R nobody /bones
 USER nobody
+WORKDIR /bones
 CMD ["/bones/scripts/bones-worker.py", "worker"]
--- bones/tasks/tasks.py
+++ bones/tasks/tasks.py
@@ -1,18 +1,25 @@
 import os
-from celery import Celery
+import sys
 
+import inspect
 import uuid
 import json
+import pprint
 
+from celery import Celery, Task
 from bones import *
 from bones.utils import *
 from bones.fast import FastA
 from bones.fastqc import FastQC
 from bones.samfile import Samfile
+from bones.sickle import Sickle
 import ssw
+
+__all__ = ["app", "Context"]
 
 default_celery_broker = "amqp://guest@broker"
 amqp_url = os.getenv("CELERY_BROKER", default_celery_broker)
+bones_output_dir = os.getenv("BONES_OUTPUT_DIR", "/tmp")
 
 app = Celery(
     __name__,
@@ -26,195 +33,221 @@ app = Celery(
     #CELERY_TASK_RESULT_EXPIRES=3600,
 #)
 
+def verify_link(source, target):
+    try:
+        return os.readlink(target) == source
+    except OSError:
+        pass
+    return False
+
+def symlink(source, directory):
+    if not source:
+        return None
+    source = os.path.abspath(source)
+    (path, filename) = os.path.split(source)
+    target = os.path.join(directory, filename)
+    if source == target:
+        return target
+    if not verify_link(source, target):
+        try:
+            os.symlink(source, target)
+        except Exception, err:
+            message = "Failed to symlink source '%s' to target '%s' (%s)" % (source, target, err)
+            raise RuntimeError(message)
+    return target
 
 class Context(dict):
-    Directories = ["reference", "qc", "alignment", "consensus", "reads", "logs"]
-    # reference=None, reads=None, mates=None, dir_output=None
-
-    def __init__(self, context=None, **kw):
-        context = context if context != None else {}
+    def __init__(self, *args, **kw):
         super(Context, self).__init__()
+        self.init(*args, **kw)
+
+    def init(self, context=None, **kw):
+        context = context if context != None else {}
         self.update(context)
         self.update(kw)
+        if "_history_stack" not in self:
+            self._history_stack = []
 
-    def update(self, ns):
-        for key in ns:
-            self.__setattr__(key, ns[key])
+    def push(self):
+        ctxt = self.copy()
+        del ctxt["_history_stack"]
+        self["_history_stack"].append(ctxt)
+
+    def pop(self):
+        assert len(self._history_stack), "pop requested on empty history stack"
+        ctxt = self._history_stack.pop()
+        ctxt["_history_stack"] = self._history_stack
+        self.clear()
+        self.init(ctxt)
+
+class ContextualizedTask(Task):
+    context = None
+
+    def bind_context(self, context):
+        self.__dict__["context"] = context
+
+    def unbind_context(self):
+        context = self.__dict__["context"]
+        self.__dict__["context"] = None
+        return context
+
+    def run(self, context=None, **kw):
+        self.bind_context(Context(context))
+        self.init()
+        ret = self._run(**kw)
+        context = self.unbind_context()
+        ret = ret if ret != None else context
+        return ret
+    
+    def init(self):
+        pass
+    
+    def _run(self, *args, **kw):
+        raise RuntimeError("You need to overload this method")
 
     def __getattr__(self, key):
-        nkey = ('_' + key) if key[0] != '_' else key
-        print("__getattr__(" + nkey + ")")
-        if nkey in self:
-            return self[nkey]
-        raise AttributeError(key)
+        context = self.__dict__.get("context", None)
+        if context != None and key in context:
+            return context[key]
+        return getattr(super(ContextualizedTask, self), key)
 
     def __setattr__(self, key, value):
-        nkey = ('_' + key) if key[0] != '_' else key
-        print("__setattr__(" + nkey + ")")
-        self[nkey] = value
+        context = self.__dict__.get("context", None)
+        if context != None:
+            context[key] = value
+        else:
+            super(ContextualizedTask, self).__setattr__(key, value)
 
-    def init_directories(self):
+class BoneTask(ContextualizedTask):
+    TaskName = "BoneTask"
+    Directories = []
+
+    def init(self):
+        if "runid" not in self.context:
+            self.runid = str(uuid.uuid4()).split('-')[0]
+        if "dir_output" not in self.context:
+            self.dir_output = os.path.join(bones_output_dir, self.runid)
+        elif os.path.isabs(self.dir_output):
+            self.dir_output = os.path.join(bones_output_dir, self.dir_output)
+        if not os.path.isdir(self.dir_output):
+            os.makedirs(self.dir_output)
         for dirname in self.Directories:
             dirpath = os.path.join(self.dir_output, dirname)
             if not os.path.isdir(dirpath):
                 os.makedirs(dirpath)
-            key = "_dir_%s" % dirname
-            self[key] = dirpath
+            key = "dir_%s" % dirname
+            setattr(self, key, dirpath)
+        self._init()
 
-    def verify_link(self, source, target):
-        try:
-            return os.readlink(target) == source
-        except OSError:
-            pass
-        return False
+    def _init(self):
+        pass
 
-    def symlink(self, source, directory):
-        if not source:
-            return None
-        source = os.path.abspath(source)
-        (path, filename) = os.path.split(source)
-        target = os.path.join(directory, filename)
-        if not self.verify_link(source, target):
-            os.symlink(source, target)
-        return target
+class BWA_IndexTask(BoneTask):
+    TaskName = "bwa_index"
+    Directories = ["reads", "reference"]
 
-    def init(self):
-        if "_runid" not in self:
-            self["_runid"] = str(uuid.uuid4()).split('-')[0]
-        if "_dir_output" not in self:
-            self["_dir_output"] = self.runid
-        #
-        if not os.path.isdir(self.dir_output):
-            os.makedirs(self.dir_output)
-        self.init_directories()
-        # files
-        self.reads = [self.symlink(fn, self.dir_reads) for fn in self.reads]
-        self.reference = self.symlink(self.reference, self.dir_reference)
-        # filenames
-        self.fn_alignment = os.path.join(self.dir_alignment, "alignment.sam")
-        self.fn_consensus = os.path.join(self.dir_consensus, "consensus.sam")
-        self.fn_results_json = os.path.join(self.dir_output, "results.json")
-        self.fn_results_txt = os.path.join(self.dir_output, "results.txt")
+    def _init(self):
+        self.reference = symlink(self.reference, self.dir_reference)
 
-    def build_index(self):
-        index = bwa.Index(reference=self.reference)
+    def _run(self, *args, **kw):
+        cmdkw = {"reference": self.reference}
+        cmdkw.update(kw)
+        index = bwa.Index(**cmdkw)
         index.run(wait=True)
         self.index_prefix = index.prefix
 
-    def align(self):
-        if not is_stale(self.reference, self.fn_alignment):
-            return
-        kw = {
+class BWA_AlignmentTask(BoneTask):
+    Directories = ["alignment"]
+    TaskName = "bwa_align"
+
+    def _init(self):
+        self.reads = [symlink(fn, self.dir_reads) for fn in self.reads]
+        self.fn_alignment = os.path.join(self.dir_alignment, "alignment.sam")
+
+    def _run(self, *args, **kw):
+        #if not is_stale(self.reference, self.fn_alignment):
+        #   return
+        cmdkw = {
             "prefix": self.reference,
             "reads": self.reads[0],
         }
         if len(self.reads) > 1:
-            kw["mates"] = self.reads[1]
-        align = bwa.Align(**kw)
+            cmdkw["mates"] = self.reads[1]
+        cmdkw.update(kw)
+        align = bwa.Align(**cmdkw)
         with open(self.fn_alignment, 'w') as fh:
             align.run(stdout=fh, wait=True)
 
-    def generate_qc(self):
+class FastQCTask(BoneTask):
+    TaskName = "fastqc"
+    Directories = ["QC"]
+
+    def _run(self, *args, **kw):
+        """
         qc_glob = os.path.join(self.dir_qc, "*")
         stale_flag = is_stale(self.reads, qc_glob)
         if self.mates:
             stale_flag |= is_stale(self.mates, qc_glob)
         if not stale_flag:
             return
+        """
         reads = [self.reads]
         if self.mates:
             reads.append(self.mates)
-        qc = FastQC(outdir=self.dir_qc, seqfiles=reads)
+        cmdkw = {
+            "outdir": self.dir_qc,
+            "seqfiles": reads,
+        }
+        cmdkw.update(kw)
+        qc = FastQC(**cmdkw)
         qc.run(wait=True)
 
-    def build_consensus(self):
-        if not is_stale(self.fn_alignment, self.fn_consensus):
-            return
-        self.align_reads()
-        samf = samfile.Samfile(self.fn_alignment)
-        cc = consensus.Consensus()
-        sequences = cc.call_samfile(samf.samf)
-        fa = FastA(sequences)
-        fa.save(self.fn_consensus)
+class SickleTask(BoneTask):
+    TaskName = "sickle"
+    Directories = ["reads"]
 
-    def compare_consensus(self):
-        refseqs = FastA.load(self.reference)
-        conseqs = FastA.load(self.fn_consensus)
-        aligner = ssw.Aligner()
-        for reference in refseqs:
-            row = []
-            for query in conseqs:
-                alignment = aligner.align(query, reference)
-                row.append(alignment)
-            row.sort(cmp=lambda x, y: cmp(x.score, y.score), reverse=True)
-            winner = row[0]
-            yield (reference, winner)
+    def _run(self, *args, **kw):
+        # XXX: gz?
+        outputs = []
+        for read_path in self.reads:
+            read_fn = os.path.split(read_path)[-1]
+            parts = read_fn.split('.')
+            read_fn = parts[0]
+            extlist = parts[1:]
+            out_fn = "%s_trimmed.%s" % (read_fn, str.join('.', extlist))
+            out_path = os.path.join(self.dir_reads, out_fn)
+            outputs.append(out_path)
+        stem = common_filename(*self.reads)
+        stem_fn = os.path.split(stem)[-1]
+        parts = read_fn.split('.')
+        stem_fn = parts[0]
+        out_fn = "%s_singles_trimmed.%s" % (stem_fn, str.join('.', extlist))
+        singles_fn = os.path.join(self.dir_reads, out_fn)
 
-    def build_report(self):
-        refs = FastA.load(self.reference)
-        report = {
-            "reference_count": len(refs),
-            "references": [],
+        # XXX: test if SE or PE mode
+        cmdkw = {
+            "mode": "pe",
+            "pe-file1": self.reads[0],
+            "pe-file2": self.reads[1],
+            "output-pe1": outputs[0],
+            "output-pe2": outputs[1],
+            "output-single": singles_fn,
         }
+        cmdkw.update(kw)
+        sickle = Sickle(**cmdkw)
+        sickle.run(wait=True)
+        self.reads = outputs
 
-        global_verified_flag = True
-        for (reference, alignment) in self.compare_consensus():
-            verified_flag = alignment.match_count == len(reference)
-            global_verified_flag &= verified_flag
-            alstat = {
-                "name": reference.name,
-                "verified": str(verified_flag),
-                "cigar": alignment.cigar,
-                "alignment": alignment.alignment_report,
-            }
-            report["references"].append(alstat)
-        report["verified"] = str(global_verified_flag)
-        return report
-    
-    def build_report_txt(self, report):
-        rpt = ''
-        for ref_report in report["references"]:
-            rpt += "Reference: %(name)s\nVerified: %(verified)s\nCIGAR: %(cigar)s\n%(alignment)s\n" % ref_report
-        return rpt
+def bootstrap_tasks():
+    thismodule = sys.modules[__name__]
+    ns = thismodule.__dict__
+    _all = ns.get("__all__", list())
+    for key in ns.keys():
+        cls = ns[key]
+        if inspect.isclass(cls) and issubclass(cls, BoneTask) and (cls.__name__ != "BoneTask"):
+            cls.bind(app)
+            ns[cls.TaskName] = cls()
+            _all.append(cls.TaskName)
+    ns["__all__"] = _all
 
-    def run(self):
-        self.init_filesystem()
-        self.generate_qc()
-        self.build_consensus()
-        report = self.build_report()
-        json_report = json.dumps(report)
-        txt_report = self.build_report_txt(report)
-        with open(self.fn_results_json, 'w') as fh:
-            fh.write(json_report)
-        with open(self.fn_results_txt, 'w') as fh:
-            fh.write(txt_report)
-
-@app.task
-def ngs_workflow(cfg):
-    ctxt = Context(cfg)
-    ctxt.init()
-    return dict(ctxt)
-
-@app.task
-def align(cfg):
-    ctxt = Context(cfg)
-    ctxt.align()
-    return dict(ctxt)
-
-@app.task
-def samtools(*args, **kw):
-    print("samtools", args, kw)
-
-@app.task
-def build_index(cfg):
-    ctxt = Context(cfg)
-    ctxt.build_index()
-    return dict(ctxt)
-
-@app.task
-def run_fastq(*args, **kw):
-    print("run_fastqc", args, kw)
-
-@app.task
-def trim_reads(*args, **kw):
-    print("trim_reads", args, kw)
+bootstrap_tasks()
